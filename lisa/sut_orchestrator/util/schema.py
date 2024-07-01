569c6f2322dc1f9bcfec636cd08f8116854ae194
smit-gardhariya <gardhariya.smit@gmail.com>
2024-07-01 17:02:40 +0530

    Add hostdev support for libvirt device passthrough
    Add hostdev support for libvirt passthrough. This is
    only for PCI device passthrough libvirt.
    
    Signed-off-by: Smit Gardhariya <sgardhariya@microsoft.com>

---
+++ lisa/sut_orchestrator/util/schema.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import List
+
+from dataclasses_json import dataclass_json
+
+
+class HostDevicePoolType(Enum):
+    PCI_NIC = "pci_net"
+    PCI_GPU = "pci_gpu"
+
+
+@dataclass_json()
+@dataclass
+class DeviceIdentifier:
+    vendor_id: str = ""
+    device_id: str = ""
+
+
+# Configuration options for device-passthrough for the VM.
+@dataclass_json()
+@dataclass
+class HostDevicePoolSchema:
+    type: HostDevicePoolType = HostDevicePoolType.PCI_NIC
+    devices: List[DeviceIdentifier] = field(default_factory=list)
